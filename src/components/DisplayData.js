25ade26b26153dfda40011f53ddaeffb73e81e38
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-06-12 16:07:06 +0300

    add fornm controlled component

---
+++ src/components/DisplayData.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function DisplayData({ firstName, lastName }) {
+    return (
+        <>
+            <h1>{firstName}</h1>
+            <h1>{lastName}</h1>
+
+        </>
+    )
+}
+
+export default DisplayData
---
+++ src/components/Parent.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import DisplayData from './DisplayData';
+import Form from './Form';
+
+function Parent() {
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
+
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value);
+    }
+    function handleLastNameChange(event) {
+        setLastName(event.target.value);
+    }
+    return (
+        <>
+            <Form firstName={firstName} lastName={lastName} handleFirstNameChange={handleFirstNameChange} handleLastNameChange={handleLastNameChange} />
+            <DisplayData firstName={firstName} lastName={lastName} />
+        </>
+    )
+}
+
+export default Parent
